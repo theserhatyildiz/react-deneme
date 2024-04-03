706be6a910b2879c8d799b1198008a99a9548202
Serhat Yildiz <yildizserhat@yahoo.com>
2024-04-03 13:39:29 +0300

    devam

--- src/componenet/Food.jsx
+++ src/componenet/Food.jsx
@@ -1,6 +1,7 @@
 import { UserContext } from "../context/UserContext";
-import { useContext } from "react"
-import Header from "./Header"
+import { useContext, useState } from "react"
+import FoodData from "./FoodData";
+
 
 export default function Food()
 {
@@ -9,34 +10,50 @@ export default function Food()
     
     const loggedData = useContext(UserContext);
 
+    const [foodItems,setFoodItems] = useState([]);
+
+    const [food,setFood] = useState(null);
 
 
     // ------------------Functions------------------
 
     function searchFood(event)
     {
-        console.log(event.target.value)
+        if(event.target.value.length!==0)
+        {
 
         // ------------------Calling the data to API------------------
 
         fetch(`http://localhost:8000/foods/${event.target.value}`, {
             method:"GET",
             headers:{
-                Authorization:`Bearer: ${loggedData.loggedUser.token}`
+                Authorization:`Bearer ${loggedData.loggedUser.token}`
             }
 
         })
         .then((respone)=>respone.json())
         .then((data)=>{
-            console.log(data)
+            if(data.message==undefined)
+            {
+                setFoodItems(data);
+            }
+            else
+            {
+                setFoodItems([]);
+            }
+            
         })
         .catch((err)=>{
             console.log(err)
         })
 
-    }
+       }
+       else 
+       {
+        setFoodItems([]);
+       }
 
-
+}
     return(
         <>
             <section className="container diet-container">
@@ -44,9 +61,39 @@ export default function Food()
                 <div className="search">
 
                     <input className="search-inp" type="search" onChange={searchFood} placeholder="Search Food" />
+                    
+                    {
+                        foodItems.length!==0?(
+
+                        <div className="search-results">
+
+                        {
+
+                            foodItems.map((item)=>{
+
+                                return(
+
+                                <p className="item" onClick={()=>{
+                                    setFood(item)
+                                }}key={item._id}>{item.NameTr}</p>
+                                )     
+                            })
+
+                        }
+
+                        </div>
+                        ):null
+                    }
 
                 </div>
 
+                {
+                food!==null?(
+                    <FoodData food = {food}/>
+                ):null
+            }
+
+            
             </section>
         </>
     )
